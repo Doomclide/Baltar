c336daa447026a29d56c81a2d015c1ec97348df1
Doomclide <65892708+Doomclide@users.noreply.github.com>
2020-10-08 17:07:18 +0300

    тут типо скрипты
    вот именно тут типо скрипты

---
+++ script.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+    void Start() {
+        Debug.Log("Hi");
+        
+    }
+
+    void Update() {
+        
+    }
+}
